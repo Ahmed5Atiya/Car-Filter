ce3a3d1eb24b8e2850cb5e59b1ce7187ab8fef47
Ahmed5Atiya <129235416+Ahmed5Atiya@users.noreply.github.com>
2024-08-25 02:18:07 +0300

    this is the next  input to search by the mode and add the icon search

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,10 +1,27 @@
 "use client";
 import { useState } from "react";
 import SearchManu from "./SearchManu";
+import Image from "next/image";
 
 function SearchBar() {
   const [menufacturer, setMenuFacturer] = useState("");
-  const handelSearch = () => {};
+  const [model, setModel] = useState("");
+  const SearchButton = ({ otherClasses }: { otherClasses: string }) => {
+    return (
+      <button type="submit" className={`ml-3 z-10 ${otherClasses}`}>
+        <Image
+          src="/magnifying-glass.svg"
+          alt="Search"
+          width={40}
+          height={40}
+          className="object-contain"
+        />
+      </button>
+    );
+  };
+  const handelSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
   return (
     <form className="searchbar" onSubmit={handelSearch}>
       <div className="searchbar__item">
@@ -12,7 +29,25 @@ function SearchBar() {
           menufacturer={menufacturer}
           setMenuFacturer={setMenuFacturer}
         />
+        <SearchButton otherClasses="sm:hidden" />
       </div>
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          alt="Search"
+          width={30}
+          height={30}
+          className="object-contain absolute left-3"
+        />
+        <input
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder="Enter You Want"
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   );
 }
